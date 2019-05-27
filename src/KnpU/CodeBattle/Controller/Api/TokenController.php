c1f38a6da750117b18e05eb534fc0fd9ec589afe
manueljun17 <manueljun17@gmail.com>
2019-05-27 20:58:28 +0800

    Validate that Token Resource

--- src/KnpU/CodeBattle/Controller/Api/TokenController.php
+++ src/KnpU/CodeBattle/Controller/Api/TokenController.php
@@ -19,6 +19,11 @@ class TokenController extends BaseController
         $data = $this->decodeRequestBodyIntoParameters($request);
         $token = new ApiToken($this->getLoggedInUser()->id);
         $token->notes = $data->get('notes');
+        
+        $errors = $this->validate($token);
+        if ($errors) {
+            $this->throwApiProblemValidationException($errors);
+        }
 
 	    $this->getApiTokenRepository()->save($token);
 	    return $this->createApiResponse($token, 201);

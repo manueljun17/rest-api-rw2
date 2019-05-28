13d80215880adf70510012f41bd3a2fba0336ee2
manueljun17 <manueljun17@gmail.com>
2019-05-28 17:40:49 +0800

    GET Your (One) Battle On

--- src/KnpU/CodeBattle/Controller/Api/BattleController.php
+++ src/KnpU/CodeBattle/Controller/Api/BattleController.php
@@ -11,6 +11,8 @@ class BattleController extends BaseController
     protected function addRoutes(ControllerCollection $controllers)
     {
         $controllers->post('/api/battles', array($this, 'newAction'));
+        $controllers->get('/api/battles/{id}', array($this, 'showAction'))
+            ->bind('api_battle_show');
     }
 
     public function newAction(Request $request)
@@ -39,8 +41,20 @@ class BattleController extends BaseController
         $battle = $this->getBattleManager()->battle($programmer, $project);
 
         $response = $this->createApiResponse($battle, 201);
-        $response->headers->set('Location', 'TODO');
+        $url = $this->generateUrl('api_battle_show', array('id' => $battle->id));
+        $response->headers->set('Location', $url);
 
+        return $response;
+    }
+
+    public function showAction($id)
+    {
+    	$battle = $this->getBattleRepository()->find($id);
+        if (!$battle) {
+            $this->throw404('No battle with id '.$id);
+        }
+
+        $response = $this->createApiResponse($battle, 200);
         return $response;
     }
 }
